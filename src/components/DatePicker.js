fb1387479bd5b211fcc3b6de5b1ca1a411f1aa4f
unknown <habib.redissi@gmail.com>
2020-10-17 11:51:54 +0100

    add date picker to navbar and responsive layout

---
+++ src/components/DatePicker.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./DatePicker.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import PeopleIcon from "@material-ui/icons/People";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+function DatePicker() {
+  const history = useHistory();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  const handleSelection = () => {};
+  return (
+    <div className="search">
+      <DateRangePicker
+        ranges={[selectionRange]}
+        onChange={handleSelection}
+        className="search__datePicker"
+      />
+      <Button onClick={() => history.push("/search")}>Search Airbnb</Button>
+    </div>
+  );
+}
+
+export default DatePicker;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import DatePicker from "./DatePicker";
 
+import PeopleIcon from "@material-ui/icons/People";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TextField from "@material-ui/core/TextField";
+import PlaceIcon from "@material-ui/icons/Place";
+import DateRangeIcon from "@material-ui/icons/DateRange";
 const Navbar = () => {
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className="navbar">
@@ -20,7 +28,47 @@ const Navbar = () => {
           </div>
         </Link>
         <div className="navbar__center">
-          <h1>navbar__center</h1>
+          <TextField
+            placeholder="When"
+            id="input-when"
+            type="search"
+            variant="outlined"
+            onClick={() => setShowSearch(!showSearch)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <DateRangeIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          {showSearch && <DatePicker />}
+          <TextField
+            placeholder="Anywhere"
+            id="input-place"
+            type="search"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PlaceIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            placeholder="1"
+            id="input-guest"
+            type="number"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PeopleIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
         </div>
         <div className="navbar__right">
           <ul clas="navbar__right__menu">
